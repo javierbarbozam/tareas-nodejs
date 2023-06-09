6a799580a29314177d165d3cfa8396263c6c868f
javierbarbozam <barboza.javier497@gmail.com>
2023-06-08 18:27:35 -0600

    change country name to country code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const rs = require("readline-sync");
 const axios = require("axios");
 
-const country = rs.question("Insert a country name");
+const countryCode = rs.question("Insert a country code");
 
 const getCountryData = async (value) => {
   try {
-    const res = await axios.get(`https://restcountries.com/v3.1/name/${value}?fullText=true`);
+    const res = await axios.get(`https://restcountries.com/v3.1/alpha/${value}`);
     console.log(res.data);
   } catch {
     console.error("An error ocurred my boy :(");
   }
 };
 
-getCountryData(country);
+getCountryData(countryCode);
